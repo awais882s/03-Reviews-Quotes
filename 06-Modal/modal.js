baf612d917286594e1b086c6b8f56c010f249126
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-02 22:22:43 +0500

    function on close btn and model buttons

--- 06-Modal/modal.js
+++ 06-Modal/modal.js
@@ -2,3 +2,12 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+const modalbtn = document.querySelector(".modal-btn ");
+const modal = document.querySelector(".modal-overlay");
+const closebtn = document.querySelector(".close-btn");
+modalbtn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+});
+closebtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
