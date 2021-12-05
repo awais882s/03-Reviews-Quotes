b5283d9a84bed84bc2665f5402d1ae989034cf55
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-05 13:58:18 +0500

    function on buttons using eachfun

---
+++ 07-Questions/Q & A.js
@@ -0,0 +1,14 @@
+// using selector inside the elements
+// traversing the dom
+
+
+
+
+
+// const btns = document.querySelectorAll(".question-btn");
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
