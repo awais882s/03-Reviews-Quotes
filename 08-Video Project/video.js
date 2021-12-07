9689398ca30fb1fa15996a34fed06406b50dd218
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-07 13:29:29 +0500

    video and play pause btn

---
+++ 08-Video Project/video.js
@@ -0,0 +1,16 @@
+// MDN
+// The DOM Content Loaded fires when the initial HTML documents has been completely loaded and parsed , without
+// waiting for stylesheet , images , and sub frames to finish
+// The load event is fired when whole page is loaded ,
+// including all dependents resources such as stylesheets and images.
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+btn.addEventListener("click", function () {
+  if (!btn.classList.contains("slide")) {
+    btn.classList.add("slide");
+    video.pause();
+  } else {
+    btn.classList.remove("slide");
+    video.play();
+  }
+});
