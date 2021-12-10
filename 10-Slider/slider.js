c8375db3681a8926b57d973a7ad515b12ff44edf
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-10 10:50:25 +0500

    prev and next btn conditions

--- 10-Slider/slider.js
+++ 10-Slider/slider.js
@@ -14,7 +14,29 @@ prevbtn.addEventListener("click", function () {
   carousel();
 });
 function carousel() {
+  // working with slides
+  // if (counter ===slides.length) {
+  //   counter = 0;
+
+  // }
+  // if (counter<0) {
+  //   counter = slides.length-1;
+
+  // }
+  if (counter < slides.length - 1) {
+    nextbtn.style.display = "block";
+  } else {
+    nextbtn.style.display = "none";
+  }
+  if (counter > 0) {
+    prevbtn.style.display = "block";
+  } else {
+    {
+      prevbtn.style.display = "none";
+    }
+  }
   slides.forEach(function (slide) {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
 }
+prevbtn.style.display = "none";
