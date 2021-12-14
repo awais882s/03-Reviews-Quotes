5dd05443a153c4105786262c20e551a514b270f2
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-14 11:52:22 +0500

    nav toggles

--- 11-Scroll/scroll.js
+++ 11-Scroll/scroll.js
@@ -36,3 +36,20 @@
 // const nmb = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 // const even = nmb.filter((n) => n % 2 !== 0);
 // console.log(even);
+
+// ==================== SCROLL PROJECT ==================================//
+
+// ******************* set date ***************************
+
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+// ********************* close links **********************
+
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  linksContainer.classList.toggle("show-links");
+});
