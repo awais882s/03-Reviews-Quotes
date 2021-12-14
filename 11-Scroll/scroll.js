6cc55fc72eb411727df664d2ed9c1d1dbae64599
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-14 15:48:29 +0500

    Update scroll.js

--- 11-Scroll/scroll.js
+++ 11-Scroll/scroll.js
@@ -80,3 +80,25 @@ window.addEventListener("scroll", function () {
     topLink.classList.remove("show-link");
   }
 });
+
+// ********************* scroll smooth *********************
+
+// select links
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    let position = element.offsetTop;
+    console.log(position);
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+  });
+});
