857fb5004bc05885deca6bd9615b12e609ddb73d
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-14 10:22:20 +0500

    even and odd functions

--- 11-Scroll/scroll.js
+++ 11-Scroll/scroll.js
@@ -29,3 +29,10 @@
 
 // const sum = (a, b) => a + b;
 // console.log(sum(50, 50));
+// filter functions even and odd function
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const odd = numbers.filter((n) => n % 2 == 0);
+console.log(odd);
+const nmb = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const even = nmb.filter((n) => n % 2 !== 0);
+console.log(even);
