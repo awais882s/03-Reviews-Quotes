6a5b3bcc07a46366f958d00e6e3e1b7595c78712
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-14 10:14:00 +0500

    arrow functions

--- 11-Scroll/scroll.js
+++ 11-Scroll/scroll.js
@@ -1,7 +1,31 @@
 // for each function examples
-const nmbrs = [2, 3, 4, 5, 6]; //array of numbers
-nmbrs.forEach((n, i, arr) => {
-  // n=elements // i=index //
-  arr[i] = n + 2;
-});
-nmbrs.forEach((n) => console.log(n));
+// const nmbrs = [2, 3, 4, 5, 6]; //array of numbers
+// nmbrs.forEach((n, i, arr) => {
+//   // n=elements // i=index //
+//   arr[i] = n + 2;
+// });
+// nmbrs.forEach((n) => console.log(n));
+// arrow functions simple method
+// const sum = function () {
+//   const a = 10;
+//   const b = 10;
+//   return a + b;
+// };
+// console.log(sum());
+// arrow functions with es6 method
+// const sum = () => {
+//   const a = 10;
+//   const b = 30;
+//   return a + b;
+// };
+// console.log(sum());
+// arrow functions another simple when one statements method
+// let a = 30;
+// let b = 20;
+// const sum = () => a + b;
+// console.log(sum());
+
+// another methods
+
+// const sum = (a, b) => a + b;
+// console.log(sum(50, 50));
