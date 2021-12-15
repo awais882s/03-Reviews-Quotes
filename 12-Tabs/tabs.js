218ac478d52003dbcecaa80be89596c32898fa51
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-15 13:59:21 +0500

    Update tabs.js

---
+++ 12-Tabs/tabs.js
@@ -0,0 +1,39 @@
+// const btns = document.querySelectorAll(".tab-btn");
+// const about = document.querySelector(".about");
+// const articles = document.querySelectorAll(".content");
+// about.addEventListener("click", function (e) {
+//   // console.log(e.target.dataset.id);
+//   const id = e.target.dataset.id;
+//   if (id) {
+//     // remove selected from others buttons
+//     btns.forEach(function (btn) {
+//       btn.classList.remove("active");
+//     });
+//     e.target.classList.add("active");
+//     // hide other articles
+//     articles.forEach(function (article) {
+//       article.classList.remove("active");
+//     });
+//     const element = document.getElementById("id");
+//     element.classList.add("active");
+//   }
+// });
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
