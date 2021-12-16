a425b58f21e2307789c72bca1235ed5bb415492c
Muhammad Awais <awaisiqbal78692110@gmail.com>
2021-12-16 22:31:18 +0500

    Update count.js

--- 13-Countdown/count.js
+++ 13-Countdown/count.js
@@ -27,7 +27,13 @@ const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 // console.log(items);
 // for new current dates
-let futureDate = new Date(2021, 11, 26, 11, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDay();
+
+let futureDate = new Date(2021,11, 26, 11, 30, 0);
+// const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 // new Date(2000, 04, 09, 11, 30, 0) if you pass these values then this show
 // otherswise current date time shows
 // console.log(futureDate);
@@ -51,7 +57,7 @@ const futureTime = futureDate.getTime();
 function getRemainingTime() {
   const today = new Date().getTime();
   const t = futureTime - today;
-  console.log(t);
+  // console.log(t);
   // 1s  = 1000ms
   // 1m  = 60s
   // 1hr = 60min
@@ -71,7 +77,23 @@ function getRemainingTime() {
   // console.log(hours);
   let mintues = Math.floor((t % hours) / oneDay);
   let seconds = Math.floor((t % oneMintue) / 1000);
-
-
+  // set values array
+  const values = [days, hours, mintues, seconds];
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class=""expired> sorry, this giveaway has expired </h4>`;
+  }
 }
+// countdown
+let countdown = setInterval(getRemainingTime, 1000);
+
 getRemainingTime();
